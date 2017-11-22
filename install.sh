f3a9817f58d039bc14216753a09e52de3ecb7b0b
n4cl <devn4cl@gmail.com>
2017-11-22 18:07:31 +0900

    Add install.sh
    WSL環境でAnsibleで自身を管理させたかったが、とりあえずshellで

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt -y install language-pack-ja
+sudo apt -y install vim
+sudo apt -y install git
+
+mkdir -p ~/workspace
+cd ~/workspace
+
+git clone https://github.com/n4cl/vim-settings.git
+ln -s ~/workspace/vim-settings/.vimrc ~/.vimrc
+
+git clone https://github.com/n4cl/shell-settings.git
+ln -s ~/workspace/shell-settings/.zshrc ~/.zshrc
+
